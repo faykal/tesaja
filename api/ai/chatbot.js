1894d310f7c9be857897a4dd64333efd4215b1e6
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-12 15:37:08 +0700

    Update chatbot.js
    tess

--- api/ai/chatbot.js
+++ api/ai/chatbot.js
@@ -30,10 +30,10 @@ module.exports = {
         try {
             const { text, model } = req.query;
             if (!text || !model) return res.status(400).json({ status: false, error: 'Text and Model is required' });
-            const data = await chatBotAi(text, model)
+            const result = await chatBotAi(text, model)
             res.status(200).json({
                 status: true,
-                result: data.result
+                result
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
