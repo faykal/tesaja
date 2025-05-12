a47cfc0a1062c4a69a66cf81afd3fef20eac4c69
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-12 16:24:07 +0700

    Add files via upload

---
+++ api/download/snackvideo.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'Snackvideo',
+    desc: 'Download video on snackvideo',
+    category: 'Download',
+    params: ['url'],
+    async run(req, res) {
+        const { url } = req.query;
+        if (!url) return res.status(400).json({ status: false, error: 'Url is required' });
+        
+        try {
+            const result = await scrape.snackvideo.download(url);
+            res.status(200).json({
+                status: true,
+                result
+            });
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    }
+}
