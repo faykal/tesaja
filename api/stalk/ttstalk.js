e1b07ae8cc599a9528741053ada85ffaabdec321
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-04 22:56:03 +0700

    Update ttstalk.js

--- api/stalk/ttstalk.js
+++ api/stalk/ttstalk.js
@@ -41,7 +41,7 @@ module.exports = {
             let anu = await tiktokStalk(user);  
             res.status(200).json({
                 status: true,
-                result: anu
+                result: anu.result.user
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
