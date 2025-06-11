3ab146ec1ed3af0076fa9f51cfb67f9cbf000a16
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-11 10:50:29 +0700

    Update play.js

--- api/download/play.js
+++ api/download/play.js
@@ -1,6 +1,4 @@
-const yts = require('yt-search');
-const fetch = require('node-fetch')
-
+const axios = require("axios");
 module.exports = {
     name: 'YouTube Play',
     desc: 'Play song on youtube',
@@ -10,14 +8,10 @@ module.exports = {
         const { q } = req.query;
         if (!q) return res.status(400).json({ status: false, error: 'Url is required' });
         try {
-            const lagu = await yts(q)
-            const vid = lagu.videos[0]
-            const link = vid.url
-            const response = await fetch(`https://ytdlpyton.nvlgroup.my.id/download/audio/?url=${link}&mode=url`)
-            const fay = response.download_url
+            const fay = await axios.get(`https://api.kenshiro.cfd/api/downloader/play?q=${q}`)
             res.status(200).json({
                 status: true,
-                data: fay
+                data: fay.data.data
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
