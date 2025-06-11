56b73802c01e4aac57e2c5f854557d4c6a623faa
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-11 10:14:16 +0700

    Update play.js

--- api/download/play.js
+++ api/download/play.js
@@ -1,4 +1,6 @@
-const { search, ytmp3, ytmp4, ytdlv2, channel } = require('@vreden/youtube_scraper');
+const yts = require('yt-search');
+const fetch = require('node-fetch')
+
 module.exports = {
     name: 'YouTube Play',
     desc: 'Play song on youtube',
@@ -8,15 +10,13 @@ module.exports = {
         const { q } = req.query;
         if (!q) return res.status(400).json({ status: false, error: 'Url is required' });
         try {
-            const result = await search(q)
-            const link = result.results[0].url
-            const fay = await ytmp3(link)
+            const res = await yts(q)
+            const vid = res.videos[0]
+            const response = await fetch(`https://ytdlpyton.nvlgroup.my.id/download/audio/?url=${vid.url}&mode=url`)
+            const fay = response.data
             res.status(200).json({
                 status: true,
-                data: {
-                    metadata: fay.metadata,
-                    download: fay.download
-                }
+                data: fay
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
