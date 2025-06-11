8e1bf14d9afe96df137b070f6d41f33473ca3297
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-11 10:25:46 +0700

    Update play.js

--- api/download/play.js
+++ api/download/play.js
@@ -12,9 +12,12 @@ module.exports = {
         try {
             const lagu = await yts(q)
             const vid = lagu.videos[0]
+            const link = vid.url
+            const response = await fetch(`https://ytdlpyton.nvlgroup.my.id/download/audio/?url=${link}&mode=url`)
+            const fay = response.download_url
             res.status(200).json({
                 status: true,
-                data: vid
+                data: fay
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
