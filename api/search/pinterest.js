e407c324c121899b27bc4b60f10181b360a64832
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-11 17:56:21 +0700

    Update pinterest.js

--- api/search/pinterest.js
+++ api/search/pinterest.js
@@ -1,4 +1,20 @@
-const axios = require("axios")
+const fetch = require('node-fetch')
+
+let pint = async (query) => {
+    const response = await fetch("https://www.pinterest.com/resource/BaseSearchResource/get/?data=" + encodeURIComponent('{"options":{"query":"' + encodeURIComponent(query) + '"}}'), {
+        "headers": {
+            "screen-dpr": "4",
+            "x-pinterest-pws-handler": "www/search/[scope].js",
+        },
+        "method": "head"
+    })
+    if (!response.ok) throw Error (`error ${response.status} ${response.statusText}`)
+    const rhl = response.headers.get("Link")
+    if (!rhl) throw Error (`hasil pencarian ${query} kosong`)
+    const links = [ ...rhl.matchAll(/<(.*?)>/gm)].map(v => v[1])
+    return links
+}
+
 module.exports = {
     name: 'Pinterest',
     desc: 'Search image on pinterest',
@@ -8,10 +24,10 @@ module.exports = {
             try {
                 const { q } = req.query;
                 if (!q) return res.status(400).json({ status: false, error: 'Query is required' });
-                const fay = await axios.get(`https://api.nekorinn.my.id/search/pinterest?q=${q}`)
+                const fay = await pint(q)
                 res.status(200).json({
                     status: true,
-                    result: fay.data.result
+                    data: fay
                 });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
