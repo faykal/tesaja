c46bb185aa54873318630a9e5c73d76507b8d764
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-15 17:30:12 +0700

    Update cuaca.js

--- api/search/cuaca.js
+++ api/search/cuaca.js
@@ -1,61 +1,35 @@
-const axios = require("axios")
-const cheerio = require('cheerio');
+const fetch = require("node-fetch")
 
-const lang = {
-  en: "weather+in+{query}",
-  id: "cuaca+di+{query}"
-};
-const proxyUrls = ["https://files.xianqiao.wang/"];
-const google = {
-  weather: async (query, language = "id", tempUnit = "C") => {
-    const url = `https://www.google.com/search?hl=${language}&lr=lang_en&q=${lang[language].replace("{query}", query.replace(" ", "+"))}`;
-    const headers = {
-      "User-Agent": "Mozilla/5.0"
-    };
+async function getWeatherInfo(location) {
     try {
-      const {
-        data
-      } = await axios.get(proxyUrls + url, {
-        headers: headers
-      });
-      const $ = cheerio.load(data);
-      const [temperature, humidity, wind, condition, location] = [$("#wob_tm").text(), $("#wob_hm").text(), $("#wob_ws").text(), $("#wob_dc").text(), $("span.BBwThe, div.wob_loc, #wob_loc").first().text()];
-      if (!temperature || !humidity || !wind || !condition || !location) {
-        throw new Error(`Missing data: ${[ "temperature", "humidity", "wind", "condition", "location" ].filter(f => !eval(f)).join(", ")}`);
-      }
-      const tempUnitSpan = $('div.vk_bk.wob-unit span[aria-disabled="true"]').text();
-      let tempValue = parseFloat(temperature);
-      const sourceUnit = tempUnitSpan.replace("°", "").toUpperCase();
-      if (sourceUnit === "F") tempValue = (tempValue - 32) * 5 / 9;
-      if (sourceUnit === "K") tempValue = tempValue - 273.15;
-      if (tempUnit === "F") tempValue = (tempValue * 9 / 5 + 32).toFixed(1);
-      if (tempUnit === "K") tempValue = (tempValue + 273.15).toFixed(1);
-      return {
-        suhu: `${tempValue}°${tempUnit}`,
-        humidity: humidity,
-        wind: wind.replace(/km\/h/, "kmh").replace(/mph/, "mph"),
-        condition: condition,
-        location: location
-      };
-    } catch (error) {
-      throw new Error(error.response ? error.response.status : error.message);
-    }
-  }
-};
+        let res = await fetch(API('https://api.openweathermap.org', '/data/2.5/weather', {
+                q: location,
+                units: 'metric',
+                appid: '060a6bcfa19809c2cd4d97a212b19273'
+            }))
 
+        if (!res) {
+            console.log('Data cuaca tidak tersedia');
+            return;
+        }
+    
+    } catch (error) {
+        console.error('[❗] Terjadi kesalahan saat mengambil data cuaca:', error);
+    }
+}
 module.exports = {
     name: 'Cuaca',
     desc: 'Search info cuaca',
     category: 'Search',
-    params: ['kota'],
+    params: ['q'],
     async run(req, res) {
         try {
-            const { kota, language = "id", tempUnit = "C" } = req.query;
-            if (!kota) return res.status(400).json({ status: false, error: 'Query is required' });
-            const weatherData = await google.weather(kota, language, tempUnit);
+            const { q } = req.query;
+            if (!q) return res.status(400).json({ status: false, error: 'Query is required' });
+            const fay = await getWeatherInfo(q)
             res.status(200).json({
                 status: true,
-                data: weatherData
+                data: fay
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
