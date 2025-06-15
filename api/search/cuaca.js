94316c02bf67b338bb790f0b21d5870f3bee9d39
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-15 17:34:00 +0700

    Update cuaca.js

--- api/search/cuaca.js
+++ api/search/cuaca.js
@@ -1,22 +1,21 @@
 const fetch = require("node-fetch")
 
 async function getWeatherInfo(location) {
-    try {
-        let res = await fetch(API('https://api.openweathermap.org', '/data/2.5/weather', {
-                q: location,
-                units: 'metric',
-                appid: '060a6bcfa19809c2cd4d97a212b19273'
-            }))
-
-        if (!res) {
-            console.log('Data cuaca tidak tersedia');
-            return;
-        }
-    
-    } catch (error) {
-        console.error('[❗] Terjadi kesalahan saat mengambil data cuaca:', error);
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`;
+    let res = await fetch(url);
+    if (!res.ok) {
+      console.log('Data cuaca tidak tersedia');
+      return null;
     }
+    let json = await res.json();
+    return json;
+  } catch (error) {
+    console.error('[❗] Terjadi kesalahan saat mengambil data cuaca:', error);
+    return null;
+  }
 }
+
 module.exports = {
     name: 'Cuaca',
     desc: 'Search info cuaca',
