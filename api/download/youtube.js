ae2cfc328a3c5d0946e31f1ff09955a7a1bb8744
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-20 17:34:04 +0700

    Update youtube

--- api/download/youtube.js
+++ api/download/youtube.js
@@ -60,12 +60,9 @@ module.exports = {
     name: 'YouTube',
     desc: 'Download from youtube',
     category: 'Downloader',
-    params: ['url'],
     async run(req, res) {
-        const { url } = req.query;
-        if (!url) return res.status(400).json({ status: false, error: 'Url is required' });
         try {
-            const fay = await ytdl(url, 'best')
+            const fay = await ytdl('https://youtube.com/watch?v=qdpXxGPqW-Y', 'best')
             res.status(200).json({
                 status: true,
                 data: fay
