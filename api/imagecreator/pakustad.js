d21765ce1bcc9b7cedd6db1a38328ef11af4974d
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-20 13:56:38 +0700

    Add files via upload

---
+++ api/imagecreator/pakustad.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+module.exports = {
+    name: 'Pak Ustad',
+    desc: 'Only type1 and type2 available',
+    category: 'Image Creator',
+    params: ['text', 'type'],
+    async run(req, res) {
+        try {
+    const { text, type } = req.query;
+    if (!text || !type) return res.status(400).json({ status: false, error: 'Text and type is required' });
+            const response = await axios.post('https://lemon-welcome.vercel.app/api/generate-image', {
+                text,
+                option: type // hanya tersedia type1 dan type2
+                }, { responseType: 'arraybuffer' })
+            let videoBuffer = response.data;
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': videoBuffer.length,
+            });
+            res.end(videoBuffer);
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    }
+};
