ee57f4fed5c49fcdc23e71623c7237a6a4067cc0
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-06-25 10:10:44 +0700

    Update play

--- api/download/play.js
+++ api/download/play.js
@@ -92,14 +92,17 @@ module.exports = {
     params: ['q'],
     async run(req, res) {
         const { q } = req.query;
-        if (!q) return res.status(400).json({ status: false, error: 'Url is required' });
+        if (!q) return res.status(400).json({ status: false, error: 'Query is required' });
         try {
             const searchResults = await search(q);
             const firstResultUrl = searchResults[0].url;
             const downloadInfo = await download(firstResultUrl);
             res.status(200).json({
                 status: true,
-                data: downloadInfo
+                data: {
+                    metadata: firstResultUrl,
+                    download: downloadInfo
+                }
             });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
